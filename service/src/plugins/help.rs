14d15a0a4510a0e314e5958006a206b72fd9a8fe
Michael Aaron Murphy <mmstick@pm.me>
2021-11-24 17:56:37 +0100

    chore(license): Relicense library + service to MPL-2.0
    As the sole author of the library and service, I elect to relicense the
    code that I have written from GPL-3.0 to MPL-2.0. This is to preserve
    copyleft-ability while permitting linking of pop-launcher's libraries.
    
    The launcher binary and its plugins shall remain as GPL-3.0, as they are
    separate binaries with no need for linking to a frontend.

--- service/src/plugins/help.rs
+++ service/src/plugins/help.rs
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: GPL-3.0-only
-// Copyright © 2021 System76
+// Copyright 2021 System76 <info@system76.com>
+// SPDX-License-Identifier: MPL-2.0
 
 use crate::*;
 use pop_launcher::*;
