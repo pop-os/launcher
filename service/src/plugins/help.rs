a5c256965459b88ef585c0382823ee91228f725c
Paul Delafosse <paul.delafosse@protonmail.com>
2022-05-12 16:42:56 +0200

    feat: add a history attribute to plugin config as described in #110 (#112)
    * feat: add a history attribute to plugin config as described in #110
    
    closes #110
    
    * fix: Correct grammar mistake in comment
    
    Co-authored-by: Jacob Kauffmann <jacobgkau@users.noreply.github.com>

--- service/src/plugins/help.rs
+++ service/src/plugins/help.rs
@@ -23,6 +23,7 @@ pub const CONFIG: PluginConfig = PluginConfig {
         regex: None,
     },
     icon: Some(IconSource::Name(Cow::Borrowed("system-help-symbolic"))),
+    history: false
 };
 pub struct HelpPlugin {
     pub id: usize,
