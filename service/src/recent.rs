24223fc6b330761d7c1d0be97f4271a297814e8e
Thomas Ruprecht <thomas.ruprecht@tu-dresden.de>
2022-07-27 22:03:26 +0200

    serialize list of recently used apps

--- service/src/recent.rs
+++ service/src/recent.rs
@@ -1,7 +1,8 @@
-use std::collections::{HashMap, hash_map::DefaultHasher};
+use std::collections::{HashMap, hash_map::DefaultHasher, VecDeque};
 use std::hash::{Hasher, Hash};
-use serde::{Deserialize, Serialize, Serializer, Deserializer};
+use serde::{Deserialize, Serialize};
 
+const N_RECENT_ITEMS: usize = 20;
 
 // Holds a long term storage that tracks how often a search
 // result was activated, and a short term storage that stores
@@ -9,11 +10,10 @@ use serde::{Deserialize, Serialize, Serializer, Deserializer};
 // vales are more recent).
 // Keys for both mappings are hashes of the acvtivated result's
 // command string.
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Serialize, Deserialize)]
 pub struct RecentUseStorage {
     long_term: HashMap<usize, usize>,
-    short_term: HashMap<usize, usize>,
-    short_term_queries: usize
+    short_term: VecDeque<usize>
 }
 
 
@@ -29,36 +29,19 @@ impl RecentUseStorage {
         let key = hash_key(exec);
         let count = self.long_term.entry(key).or_insert(0);
         *count += 1;
-
-        self.short_term_queries += 1;
-        self.short_term.insert(key, self.short_term_queries);
+        self.short_term.push_back(key);
+        if self.short_term.len() > N_RECENT_ITEMS {
+            self.short_term.pop_front();
+        }
     }
 
     pub fn get_recent<K: Hash>(&self, exec: &K) -> usize {
-        self.short_term.get(&hash_key(exec)).copied().unwrap_or(0)
+        self.short_term.iter().enumerate().rev().filter_map(
+            |(i, k)| if *k == hash_key(exec) { Some(i+1) } else { None }
+        ).next().unwrap_or(0)
     }
 
     pub fn get_freq<K: Hash>(&self, exec: &K) -> usize {
         self.long_term.get(&hash_key(exec)).copied().unwrap_or(0)
-    }
-}
-
-impl Serialize for RecentUseStorage {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        // Only serialize the long term storage
-        HashMap::serialize(&self.long_term, serializer)
-    }
-}
-
-impl<'de> Deserialize<'de> for RecentUseStorage {
-    fn deserialize<D>(deserializer: D) -> Result<RecentUseStorage, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let lt = HashMap::deserialize(deserializer)?;
-        Ok(RecentUseStorage{ long_term: lt, short_term: HashMap::new(), short_term_queries: 0})
     }
 }
