a35a1a9c3993905f0f48e55b88bc187f3eb3818a
pwygab <88221256+merelymyself@users.noreply.github.com>
2022-09-30 00:23:18 +0800

    Add poweroff keyword to `session-shutdown.sh`

--- scripts/session/session-shutdown.sh
+++ scripts/session/session-shutdown.sh
@@ -3,6 +3,6 @@
 # name: Power off
 # icon: system-shutdown
 # description: Shut down the system
-# keywords: power off shutdown
+# keywords: power off shutdown poweroff
 
 gnome-session-quit --power-off
