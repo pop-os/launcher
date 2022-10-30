f92a47f8d2e27f10d52b4c901a63b71ae9587128
Ron Waldon <jokeyrhyme@jokeyrhy.me>
2022-10-31 06:05:48 +1100

    fix: non-GNOME systemd fallbacks for session scripts

--- scripts/session/session-logout.sh
+++ scripts/session/session-logout.sh
@@ -1,8 +1,20 @@
-#!/bin/sh
+#!/usr/bin/sh
 #
 # name: Log Out
 # icon: system-log-out
 # description: Log out to the login screen
 # keywords: log out logout
 
-gnome-session-quit --logout
+set -eu
+
+is_gnome() {
+  command -v dbus-send >/dev/null && \
+    command -v gnome-session-quit >/dev/null && \
+    dbus-send --print-reply --dest=org.gnome.Shell /org/gnome/Shell org.freedesktop.DBus.Properties.Get string:org.gnome.Shell string:ShellVersion >/dev/null 2>&1
+}
+
+if is_gnome; then
+  gnome-session-quit --logout
+elif command -v loginctl >/dev/null; then
+  loginctl terminate-user "${USER}"
+fi
