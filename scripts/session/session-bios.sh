f1df125706c2a4fc4b2635648ba6752b6bcc3595
Ashley Wulber <ashley@system76.com>
2025-01-21 13:18:03 -0500

    feat: confirm enter BIOS with cosmic-osd

--- scripts/session/session-bios.sh
+++ scripts/session/session-bios.sh
@@ -13,7 +13,13 @@ is_gnome() {
     dbus-send --print-reply --dest=org.gnome.Shell /org/gnome/Shell org.freedesktop.DBus.Properties.Get string:org.gnome.Shell string:ShellVersion >/dev/null 2>&1
 }
 
-if is_gnome; then
+is_cosmic() {
+  command -v cosmic-osd >/dev/null && [ "$XDG_SESSION_DESKTOP" = "COSMIC" ]
+}
+
+if is_cosmic; then
+  cosmic-osd enter-bios
+elif is_gnome; then
   systemctl reboot --firmware-setup
 elif command -v systemctl >/dev/null; then
   systemctl reboot --firmware-setup
